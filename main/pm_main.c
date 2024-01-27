d6f62b3b55e5c1808d35585ad36baca3f12cb64a
Matt <mrd@mrd.space>
2024-01-27 09:14:44 -0500

    switching from influx to mqtt

--- main/pm_main.c
+++ main/pm_main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <string.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -17,6 +18,8 @@
 #include "esp_sleep.h"
 #include "esp_mac.h"
 
+#include "mqtt_client.h"
+
 #define TAG "plantmonitoring"
 #define TRUE (1==1)
 #define FALSE (!TRUE)
@@ -24,6 +27,7 @@
 #define PM_ADC_BIT_WIDTH ADC_WIDTH_BIT_DEFAULT
 #define uS_TO_S_FACTOR (uint64_t)1000000
 #define PM_MEASURE_EVERY_MINS (uint64_t)60
+#define CONFIG_BROKER_URL "mqtt://192.168.1.15"
 
 mac_to_station **stations = NULL;
 mac_to_station *config = NULL;
@@ -78,6 +82,7 @@ void sync_time(void)
     ESP_LOGI(TAG, "The current date/time is: %s", strftime_buf);
 }
 
+
 void load_config(void) {
     uint8_t mac[8] = { 0 };
     char macstr[24] = { '\0' };
@@ -102,6 +107,73 @@ void load_config(void) {
     }
     assert(config != NULL);
     ESP_LOGI(TAG, "My station name: %s", config->station);
+}
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+esp_mqtt_client_handle_t mqtt_init(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = CONFIG_BROKER_URL,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+    return client;
 }
 
 void app_main(void)
@@ -142,6 +214,10 @@ void app_main(void)
     // Connect to WiFi
     wifi_init();
 
+    esp_mqtt_client_handle_t mqtt_client;
+    mqtt_client = mqtt_init();
+
+
     // Synchronize time
     sync_time();
     
@@ -153,7 +229,7 @@ void app_main(void)
 
     // Configure ADC channels
     adc1_config_width((adc_bits_width_t)ADC_WIDTH_BIT_DEFAULT);
-    adc1_config_channel_atten(ADC1_CHANNEL_2, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11);
     adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11);
     adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
     
@@ -163,7 +239,7 @@ void app_main(void)
 
     while (TRUE) {
         for (int i = 0; i < 5; i++) {
-            adc1_reading[0][i] = adc1_get_raw(ADC1_CHANNEL_2);
+            adc1_reading[0][i] = adc1_get_raw(ADC1_CHANNEL_5);
             adc1_reading[1][i] = adc1_get_raw(ADC1_CHANNEL_3);
             adc1_reading[2][i] = adc1_get_raw(ADC1_CHANNEL_4);
         }
@@ -173,14 +249,25 @@ void app_main(void)
                 adc1_values[i] += adc1_reading[i][o];
             }
             adc1_values[i] = adc1_values[i]/5;
-            ESP_LOGI(TAG_CH[i], "avg=%d", adc1_values[i]);    
+            ESP_LOGI(TAG_CH[i], "avg=%d", adc1_values[i]);
+            if (mqtt_client) {
+                char* topic;
+                topic = "plants/monitor";
+                ESP_LOGI(TAG_CH[i], "publishing %d to topic %s", adc1_values[i], topic);
+                int published_bytes = esp_mqtt_client_publish(mqtt_client, topic, sprintf("%d", adc1_values[i]), 0, 0, 0); // sizeof(adc1_values[i]), 0, 0);
+                //int published_bytes = mqtt_client->publish(topic, sprintf("%d", adc1_values[i]), 0, 0, 0);
+                ESP_LOGI(TAG_CH[i], "published_bytes=%d", published_bytes);
+            } else {
+                ESP_LOGI(TAG_CH[i], "No mqtt client, cannot publish data");
+            }
         }
         for (int o = 0; o < 3; o++) {
             char value[14] = { '\0' };
                 
             snprintf(value, 8, "%u", 4095 - calculate_vdc_mv(adc1_values[o]));
 
-            write_influxdb(config->station, TAG_CH[o], value);
+
+            // write_influxdb(config->station, TAG_CH[o], value);
         }
         printf("Entering deep sleep...\n");
         esp_deep_sleep(PM_MEASURE_EVERY_MINS * 60 * uS_TO_S_FACTOR);
@@ -188,6 +275,7 @@ void app_main(void)
 
         wifi_init();
         sync_time();
+        mqtt_client = mqtt_init();
     }
     printf("Restarting now.\n");
     fflush(stdout);
